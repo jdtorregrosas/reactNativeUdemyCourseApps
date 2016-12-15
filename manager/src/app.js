c73de2734c409454a3b42dc6401aeaf08e9d8b1c
jdtorregrosas <jdtorregrosas@gmail.com>
2016-12-15 15:36:53 -0500

    Add more boilerplate things

---
+++ manager/src/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBeOK0zs6i3gcWV3EMCOpJRloZ3yYkJ2BA',
+      authDomain: 'manager-f672d.firebaseapp.com',
+      databaseURL: 'https://manager-f672d.firebaseio.com',
+      storageBucket: 'manager-f672d.appspot.com',
+      messagingSenderId: '877503675449'
+    };
+    firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
